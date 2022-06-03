0e8a7d0bc1fc06d69b4380c21f3414f6605d0efa
Wareis <reiskofi@gmail.com>
2022-06-03 01:07:26 +0000

    Print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,12 @@
 
 /**
  * This is a description.
+ *
+ * main is void.
+ * return ia zero.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
