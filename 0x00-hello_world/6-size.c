e6f1079a0493673ea2b3570288421ea7ac3e3345
Wareis <reiskofi@gmail.com>
2022-06-03 03:36:03 +0000

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,11 @@ int main(void)
 {
 int intType;
 float floatType;
-long int lint;
-long long int llint;
 char charType;
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(lint));
-printf("Size of double: %lld bytes\n", sizeof(llint));
+printf("Size of long int: %ld bytes\n", sizeof(long int));
+printf("Size of long long int: %lld bytes\n", sizeof(long long int));
 printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
 }
