8526a7a28c33a4afeb27cc106bc4427bfccb942a
Wareis <reiskofi@gmail.com>
2022-06-04 22:57:55 +0000

    rev alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nv;
+for (nv = 'z'; nv <= 'a'; nv++)
+{
+putchar(nv);
+}
+putchar('\n');
+retunr (0);
+}
