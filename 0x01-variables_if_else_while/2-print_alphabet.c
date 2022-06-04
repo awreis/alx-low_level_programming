9fb15726215cea37db695994c698baa784f843ed
Wareis <reiskofi@gmail.com>
2022-06-04 02:07:26 +0000

    lower alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int k;
+for (k = 'a'; k <= 'x'; k++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
