38099b94beec69d82763085f8505845f970ebac2
Wareis <reiskofi@gmail.com>
2022-06-04 22:14:03 +0000

    rand num2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 5)
+printf("Last digit of %d is %d and is ", n, (n%10));
+if ((n%10) > 5)
 {
-printf("Last digit of %d is 8 and is greater than 5\n", n);
+printf("greater than 5\n");
 }
-if (n == 0)
+else if((n%10) == 0)
 {
-printf("Last digit of %d is 0 and is 0\n", n);
+printf("0\n");
+}
+else
+{
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
