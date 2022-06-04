2cb922228337463a5c112f94b313108cdfb8c4b8
Wareis <reiskofi@gmail.com>
2022-06-04 23:43:48 +0000

    end man

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nc;
+for (nc = '0'; nc <= '9'; nc++)
+{
+putchar(nc);
+if (n != '9')
+{
+putchar(',');
+putchar('');
+}
+}
+putchar('\n');
+return (0);
+}
