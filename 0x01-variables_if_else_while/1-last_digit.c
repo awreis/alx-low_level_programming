ff2c02e70e7a27bf3989498b26bccbbc78f315bc
Wareis <reiskofi@gmail.com>
2022-06-04 01:41:28 +0000

    Random num 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("Last digit of %d is 8 and is greater than 5\n");
+printf("Last digit of %d and is greater than 5\n");
 }
 else if (n == 0)
 {
-printf("Last digit of %d is 0 and is 0\n");
+printf("Last digit of %d and is 0\n");
 }
 else if (n < 6 && n != 0)
 {
-printf("Last digit of %d is -8 and is less than 6 and not 0\n");
+printf("Last digit of %d and is less than 6 and not 0\n");
 }
 return (0);
 }
