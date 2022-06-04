714b29ef3d19f409f1cd0de61b3378d393d4ec57
Wareis <reiskofi@gmail.com>
2022-06-04 03:11:28 +0000

    lowUP

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int lowUP;
+for (lowUP = 'a'; lowUP <= 'z'; lowUP++)
+{
+putchar(lowUP);
+putchar('\n');
+}
+for (lowUP = 'A'; lowUP <= 'Z'; lowUP++)
+{
+putchar(lowUP);
+getchar();
+}
+return (0);
+}
