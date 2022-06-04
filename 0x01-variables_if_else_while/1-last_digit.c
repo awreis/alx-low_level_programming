7096a2b670aef7dd34720d7644c9fab7c72f9543
Wareis <reiskofi@gmail.com>
2022-06-04 01:06:24 +0000

    Random number2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,13 @@ if (n > 5)
 {
 printf("Last digit of %d is 8 and is greater than 5\n", n);
 }
-else if (n == 0)
-{
-printf("Last digit of %d is 0 and is 0\n", n);
-}
 else if (n < 6 && n != 0)
 {
-printf("Last digit of %d is -8 and is less than 6 and not 0\n", n);
+printf("Last digit of %d is -8 and is less than 6 and not \n", n);
+}
+else if (n == 0)
+{
+printf("Last digit of %d is 0 and is 0\n");
 }
 return (0);
 }
