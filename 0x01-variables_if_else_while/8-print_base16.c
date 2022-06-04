8e8d1dfafc42fb178574efa22eff6e5b7f2893fd
Wareis <reiskofi@gmail.com>
2022-06-04 23:23:30 +0000

    numalph

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nx;
+int xn;
+for (nx = 0; nx < 10; nx++)
+{
+putchar(nx);
+}
+for (xn = 'a'; xn < 'f'; xn++)
+{
+putchar(xn);
+}
+putchar('\n');
+return (0);
+}
