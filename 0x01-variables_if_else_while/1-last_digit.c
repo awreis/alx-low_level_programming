62ed913955ed102bf57e1332c8f1f79116b3e05f
Wareis <reiskofi@gmail.com>
2022-06-04 00:47:36 +0000

    Random numbers 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,22 +7,22 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("Last digit of %d and is greater than 5", n);
+printf("Last digit of %d is 8 and is greater than 5", n);
 }
 else if (n == 0)
 {
-printf("Last digit of %d and is 0", n);
+printf("Last digit of %d is 0 and is 0", n);
 }
 else if (n < 6 && n != 0)
 {
-printf("Last digit of %d and is less than 6 and not 0", n);
+printf("Last digit of %d is -8 and is less than 6 and not 0", n);
 }
 return (0);
 }
