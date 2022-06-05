9c678e0fbc9231d7b65968e6cc8e3fac0bd7f666
Wareis <reiskofi@gmail.com>
2022-06-05 21:28:16 +0000

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ for (a = 0; aa <= 8; a++)
 {
 for (a = 1; aa <= 9; aa++)
 {
+putchar((a % 10) + '0');
+putchar((aa % 10) + '0');
 if ((a == 1 && aa == 1) || (a == 2 && aa <= 2) || (a == 3 && aa <= 3) || (a == 4 && a <= 4) || (a == 5 && aa <= 5) || (a == 6 && aa <= 6) || (a == 7 && aa <= 7) || (a == 8 && aa <= 8))
 {
 continue;
 }
-putchar((a % 10) + '0');
-putchar((aa % 10) + '0');
 putchar(',');
 putchar(' ');
 }
