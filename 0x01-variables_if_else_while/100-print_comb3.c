3f874cf13233a5d187dc704c660d279b7c55f17e
Wareis <reiskofi@gmail.com>
2022-06-05 08:20:41 +0000

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ar, ra;
+for (ar = '1'; ar <= '9'; ar++)
+{
+for (ra = '1'; ra <= '9'; ra++)
+{
+putchar((ar / 10) + '0');
+putchar((ar % 10) + '0');
+if (ar == '9' && ra == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
