665248b08c3c2f6e976ba7f91c825ade8b4ab9f5
Wareis <reiskofi@gmail.com>
2022-06-06 20:03:09 +0000

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,28 +8,13 @@
 int main(void)
 {
 int a, aa;
-for (a = 0; aa <= 8; a++)
+for (a = '0'; aa <= '9'; a++)
 {
-for (a = 1; aa <= 9; aa++)
+for (a = '0'; aa <= '9'; aa++)
 {
-if ((a == 1 && aa <= 1) || (a == 2 && aa <= 2))
-{
-continue;
-}
-if ((a == 3 && aa <= 3) || (a == 4 && a <= 4))
-{
-continue;
-}
-if ((a == 5 && aa <= 5) || (a == 6 && aa <= 6))
-{
-continue;
-}
-if ((a == 7 && aa <= 7) || (a == 8 && aa <= 8))
-{
-continue;
-}
-putchar((a % 10) + '0');
-putchar((aa % 10) + '0');
+putchar(a);
+putchar(aa);
+if (!(a == '9' && aa == '9'))
 putchar(',');
 putchar(' ');
 }
