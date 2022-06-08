37d2cb15349b86064a9db309804db2841077bfc9
Wareis <reiskofi@gmail.com>
2022-06-08 01:58:22 +0000

    alph xx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets ten x
+ * 
+ * Return: Void
+ */
+
+void print_alphabet_x10(void)
+{
+int space;
+for (space = '0'; space <= '9'; space++)
+{
+int alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+_putchar(alph);
+}
+}
+_putchar('\n');
+}
