68bf185842c615f85692c9768b71dd41534fdc47
Wareis <reiskofi@gmail.com>
2022-06-08 10:23:52 +0000

    absolute num

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - computes value of an integer
+ * _abs - computes value of an integer x
  *
  * Return: returns 0
  */
@@ -10,6 +10,7 @@ int _abs(int x)
 {
 int y;
 y = _abs(x);
+if (x > '0' || x < '0' || x == '0')
 _putchar(y);
 return (0);
 }
