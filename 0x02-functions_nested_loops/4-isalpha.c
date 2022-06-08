7306af104fa7d2af3933fa816b58463d7e1f6480
Wareis <reiskofi@gmail.com>
2022-06-08 03:23:55 +0000

    alph

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * _isalpha - checks for an alphabet
+ *
+ * Return: returns 1 if alphabet, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
