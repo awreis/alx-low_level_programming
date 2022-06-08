fc183a3063bfd89765eae36a3b1ece890856d19b
Wareis <reiskofi@gmail.com>
2022-06-08 15:54:05 +0000

    0 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,22 +3,18 @@
 
 /**
  * print_to_98 - prints natural numbers from 0 to 98
+ * @n: integer parameter
  *
  * Return: always 0
  */
 
 void print_to_98(int n)
 {
-for (n = '0'; n <= '9'; n++)
+for (n = 0; n < 99; n++)
 {
-int x;
-for (x = '1'; x <= '8'; x++)
-{
-_putchar(x);
 _putchar(n);
 _putchar(',');
 _putchar(' ');
-}
 }
 _putchar('\n');
 }
