d9a55b5bd174d7189ccd0d25fc9d15c81a8486b2
Wareis <reiskofi@gmail.com>
2022-06-08 05:49:36 +0000

    list num

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers from 0 to 98
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+for(n = '0'; n <= '9'; n++)
+{
+int x;
+for (x = '1'; x <= '8'; x++)
+{
+_putchar(x);
+_putchar(n);
+if (n == '1' && n <= '9')
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+return (0);
+}
