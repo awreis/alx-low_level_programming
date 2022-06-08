6c40f6f0af956514f36a40de02ff00419187e04e
Wareis <reiskofi@gmail.com>
2022-06-08 10:51:00 +0000

    last dig

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+#include <time.h>
 
 /**
  * print_last_digit - prints the lst digit
- * int x - passed parameter
+ * (int x) - parameter
  *
  * Return: returns 0
  */
 
 int print_last_digit(int x)
 {
-int y;
-y = x % 10;
-_putchar(y);
+srand(time(0));
+x = rand() - RAND_MAX / 2;
+_putchar(x % 10);
 return (0);
 }
