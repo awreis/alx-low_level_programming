0408c1ddc5e66e05483e31d1887f4f952bc534b9
Wareis <reiskofi@gmail.com>
2022-06-08 02:24:39 +0000

    lowa

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _islower - checks for lower case
+ *
+ * Return: Return 1 if c is lower, otherwise 0
+ */
+
+int _islower(int c)
+{
+int c;
+if (_islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
