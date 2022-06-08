bdf8af992b6b4a889baca2a3eaf42c329d8ebca8
Wareis <reiskofi@gmail.com>
2022-06-08 13:07:52 +0000

    lastdig

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,18 @@
  * print_last_digit - prints the lst digit
  * @x: parameter
  *
- * Return: returns 0
+ * Return: returns x % 10
  */
 
 int print_last_digit(int x)
 {
-if (x < 0 || x == INT_MIN)
+if (x < 0)
 {
 x *= -1;
+}
+else if (x == INT_MIN)
+{
+x *= 0;
 }
 _putchar('0' + x % 10);
 return (x % 10);
