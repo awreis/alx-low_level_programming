c4319e40a9e9868b0875ebb66d3183c0eb728c68
Wareis <reiskofi@gmail.com>
2022-06-08 05:04:50 +0000

    9x

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes value of an integer
+ *
+ * Return: returns 0
+ */
+
+int _abs(int)
+{
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+if ((n == 0 && n <= 9) || (x == 0 && x <= 9))
+{
+int result;
+result = n*x;
+_putchar(result);
+}
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+return (0);
+}
