18c15ea78211174af8358f9dcb4b8a2006530227
Wareis <reiskofi@gmail.com>
2022-06-08 16:51:31 +0000

    0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,15 @@ void print_to_98(int n)
 {
 for ( ; n < 98; n++)
 {
-printf("%d, ", n);
+printf("%d,", n);
 }
 for ( ; n > 98; n--)
 {
-printf("%d, ", n);
+printf("%d,", n);
 }
 if (n == 98)
 {
-printf("%d ", n);
+printf("%d", n);
 }
 printf("\n");
 }
