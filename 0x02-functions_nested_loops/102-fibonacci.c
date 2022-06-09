bdad6b0b5b44f438f7c20f838f991c60f8948913
Wareis <reiskofi@gmail.com>
2022-06-09 05:01:54 +0000

    fibo1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/** 
+ * main - fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int x;
+	long u = 1;
+	long v = 2;
+
+	for (x = 1; x <= (50 / 2); x++)
+	{
+		printf("%li, %li, ", u, v);
+		u += v;
+		v += u;
+	}
+	if ((50 / 2) == 1)
+	{
+		printf("%li", x);
+	}
+	printf("\n");
+
+	return (0);
+}
