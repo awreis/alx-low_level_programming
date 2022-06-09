2353757d3df1ac5f3d3074122048e3e7dcfd7cf5
Wareis <reiskofi@gmail.com>
2022-06-09 18:18:32 +0000

    date

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,18 +16,14 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-	
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-	
 	}
 	else
 	{
 		largest = c;
 	}
-	
 	return (largest);
-
 }
