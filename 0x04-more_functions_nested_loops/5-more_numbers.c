78657947f80474b44f14bb680d88fe5f25dfbb78
Wareis <reiskofi@gmail.com>
2022-06-10 04:08:28 +0000

    more num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10x the numbers, from 0 - 14
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if ((y / 10) > 0)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_purchar('\n');
+	}
+}
