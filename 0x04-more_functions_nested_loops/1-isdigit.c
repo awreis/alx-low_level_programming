ed12508c4e2f8decfaee59982c72c5a0c3796996
Wareis <reiskofi@gmail.com>
2022-06-10 01:41:07 +0000

    is a dig

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit from 0 to 9
+ * @c: integer
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define NEST_h
 
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
