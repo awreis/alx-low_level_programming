8818a57927a500c1674c81d862dc37fdfff7500e
Wareis <reiskofi@gmail.com>
2022-06-10 04:31:56 +0000

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 	int print_straight_line;
 
-	for (print_straight_line = n; print_straight_line > 0; print_straight_line++)
+	for (print_straight_line = n; print_straight_line > 0; print_straight_line--)
 		_putchar('_');
 
 	_putchar('\n');
