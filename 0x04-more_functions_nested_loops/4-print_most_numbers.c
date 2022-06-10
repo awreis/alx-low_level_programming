e8ce7669ff53722b5d81466bfbf129377feeee04
Wareis <reiskofi@gmail.com>
2022-06-10 03:36:25 +0000

    count

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,13 @@ void print_most_numbers(void)
 {
 	int v;
 
-	v = '0';
-	while (v <= '9')
+	for (v = 0; v <= '9'; v++)
 	{
 		if (v == '2' || v == '4')
 		{
 			continue;
 		}
 		_putchar(v);
-		v++;
 	}
 	_putchar('\n');
 }
