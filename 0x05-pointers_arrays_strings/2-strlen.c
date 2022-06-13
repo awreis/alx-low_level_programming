e85b736188ebe26ba8b40f54f3a90558f0ac13da
Wareis <reiskofi@gmail.com>
2022-06-13 22:10:27 +0000

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - This fumction returns the length
+ * of a string
+ * @s: integer parameter
+ * Return: s
+ */
+
+int _strlen(char *s)
+{
+	return (_strlen(s));
+}
