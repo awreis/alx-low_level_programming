824c7e8daab64d4ccdf316f8ef5d25cc01235cca
Wareis <reiskofi@gmail.com>
2022-06-14 04:16:53 +0000

    prints string in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: integer
+ */
+
+void print_rev(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str--);
+	}
+	_putchar('\n');
+}
