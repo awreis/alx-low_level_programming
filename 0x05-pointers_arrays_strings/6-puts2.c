51d6e806cd25af668d4b4dda23bb37fa901901d6
Wareis <reiskofi@gmail.com>
2022-06-14 04:54:45 +0000

    len

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ * starting with the first character
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int x = 0, len = 0;
+
+	while (str[x++])
+	{
+		len++;
+	}
+
+	for (x = 0; x < len; x+= 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar ('\n');
+}
