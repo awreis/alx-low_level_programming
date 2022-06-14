069aa8c875097b8eec758fbd204434765d7def33
Wareis <reiskofi@gmail.com>
2022-06-14 04:27:38 +0000

    print in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,22 @@
 
 void print_rev(char *s)
 {
+	int len = 0;
+	int i;
+
 	while (*s != '\0')
 	{
-		_putchar(*s--);
+		len++;
+		++s;
+	}
+	s--;
+
+	i = len;
+	while (i > 0)
+	{
+		_putchar(*s);
+		i--;
+		s--;
 	}
 	_putchar('\n');
 }
