deaf063d9116fcedb16696dbd381ac59e843b884
Wareis <reiskofi@gmail.com>
2022-06-14 05:09:11 +0000

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+/**
+ * puts_half - prints half of a string with a \n
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int idx;
+	int len = _strlen(str);
+
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
+}
