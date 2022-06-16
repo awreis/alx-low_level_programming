6b0e369e51038b0087ad15b9dc42b5086a72b747
Wareis <reiskofi@gmail.com>
2022-06-16 20:14:16 +0000

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: a string
+ * @src: another string
+ * Return: a character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char real_dest;
+	char real_src;
+	char p;
+
+	real_dest [] = " ";
+	real_src [] = " ";
+	
+	dest = &real_dest;
+	src = &real_dest;
+
+	_strcat(*dest,*src);
+	
+	p = *dest;
+	_putchar(p);
+
+	return (char);
+}
