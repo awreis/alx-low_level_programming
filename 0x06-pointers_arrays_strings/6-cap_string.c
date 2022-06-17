c0b58c8cb0e843991ec6cf53fe722c80f597c840
Wareis <reiskofi@gmail.com>
2022-06-17 04:26:56 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words
+ * @w: string parameter
+ * Return: string
+ */
+
+char *cap_string(char *w)
+{
+	int d = 0;
+
+	if (w[d] >= 'a' && w[d] <= 'z')
+		w[d] = w[d] - 'a' +'A';
+	d++;
+
+	while (w[d] != '\0')
+	{
+		if ((w[d] >= 'a' && w[d] <= 'z') && (w[d - 1] == ',' ||
+					w[d - 1] == ';' || w[d - 1] == '.' ||
+					w[d - 1] == '!' || w[d - 1] == '?' ||
+					w[d - 1] == '"' || w[d - 1] == '{' ||
+					w[d - 1] == '}' || w[d - 1] == '(' ||
+					w[d - 1] == ')' || w[d - 1] == ' ' ||
+					w[d - 1] == '\t' || w[d - 1] == '\n'))
+			d++;
+	}
+	return (w);
+}
