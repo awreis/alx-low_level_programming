42b1c36421fbbb1a86ae8c973b646fcfb5cbc768
Wareis <reiskofi@gmail.com>
2022-06-21 04:49:00 +0000

    2D

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int r, c;
+	int r;
+	int c;
 
 	for (r = 0; r < 8; r++)
 	{
 		for (c = 0; c < 8; c++)
 			_putchar(a[r][c]);
-		_putchar('\0');
+		_putchar('\n');
 	}
 }
