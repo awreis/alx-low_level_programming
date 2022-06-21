c29c19f7a113e2c903b0638effb9f9bff6cea735
Wareis <reiskofi@gmail.com>
2022-06-21 04:14:45 +0000

    locate

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int x;
-
-	x = 0;
+	int x = 0;
 
 	while (s[x] != '\0' && s[x] != c)
 		x++;
 
-		if (s[x] == c)
-			return (&s[x]);
-		else
-			return (NULL);
+	if (s[x] == c)
+		return (&s[x]);
+	else
+		return (NULL);
 }
