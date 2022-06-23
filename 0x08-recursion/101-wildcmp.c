f36b6af86350db26be80f4f9a817cad2ade25a95
Wareis <reiskofi@gmail.com>
2022-06-23 06:00:56 +0000

    d

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
 
-	if (*s == '*')
+	if (*s22 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 
 	if (*s1 == *s2)
