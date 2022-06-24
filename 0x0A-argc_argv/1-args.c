bd0d6e0ee2cffc1d2bd2d0e9c2ff5d6d61603637
Wareis <reiskofi@gmail.com>
2022-06-24 15:34:39 +0000

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: counter integer
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
