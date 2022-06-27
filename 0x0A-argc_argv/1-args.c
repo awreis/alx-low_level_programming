107d94f70ae6d127f46757b4b9dc7d231ebc7669
Wareis <reiskofi@gmail.com>
2022-06-27 15:27:51 +0000

    cnt

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the number of arguments passed
@@ -8,7 +7,7 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] _attribute_((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
