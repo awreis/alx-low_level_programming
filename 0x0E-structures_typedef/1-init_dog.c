a1a029e0ce4fe66c380812323fe9b370088fb6cb
Wareis <reiskofi@gmail.com>
2022-07-05 06:23:14 +0000

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: member
  */
 
-void init_dog(struct dog *d, char *name, float age, char *ownwer)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
