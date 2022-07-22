de03f8297c61dff6c970a58f35562c3dff6b572f
Wareis <reiskofi@gmail.com>
2022-07-22 02:30:17 +0000

    Error Fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	difference = n ^ m;
 
-	do{
+	do {
 		diff_bits += (difference & 1);
 		difference >>= 1;
 	} while (difference > 0);
