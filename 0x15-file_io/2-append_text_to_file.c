bafb24f0c3228536164f690947c5101241d7a78e
awreis <40414863+awreis@users.noreply.github.com>
2022-07-25 21:26:34 +0000

    changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,56 @@
 #include "main.h"
 
 /**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		
+
+	return (len);
+}
+
+/**
  * append_text_to_file - appends text to the end of the file
  * @filename: file name
  * @text_content: content of the text
  * Return: 1 on success, -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int count;
+	int n_wrote;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
 
-	fd = open(filename, O_APPEND | O_WRONLY);
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	for (count = 0; text_content[count] != '\0'; count++)
+	/* if nothing to write, just return newly created file */
+	if (!text_content)
 	{
-		if (write(fd, &text_content[count], 1) == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (1);
 	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
 	close(fd);
 	return (1);
 }
