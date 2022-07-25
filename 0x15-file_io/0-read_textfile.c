1133b499f2f7657056c895bdd746af454081eac1
Wareis <reiskofi@gmail.com>
2022-07-25 19:39:38 +0000

    Error Fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,32 +15,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-
 	buf = malloc(sizeof(char) *letter);
-	if (!buf)
+	if (buf == NULL)
 		return (0);
 
-	read_file = read(fd, buf, letters);
-	if (count < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
+		free(buff);
+		return (0);
+	}
+
+	count = read(fd, buf, letters);
+	if (count == -1)
+	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
-	buf(count) = '\0';
-
+	
+	ryt = write(STDUOT_FILENO, &buf[count], 1);
+	for (ryt = 0; ryt < count; ryt++)
+	{
+		if (ryt == -1)
+		{
+			close(fd);
+			free(buf);
+			return (0);
+		}
+	}
 	close(fd);
-
-
-	ryt = write(STDUOT_FILENO, buf, count);
-	if (ryt < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
 	free(buf);
 	return (count);
 }
