e2caecc24aa72bc9ab2d95b422d0188d86c0f68a
awreis <40414863+awreis@users.noreply.github.com>
2022-07-25 21:31:25 +0000

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
  * create_file - creates a file
  * @filename: file name
  * @text_content: content of the file
@@ -9,25 +25,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, count, sl = 0;
+	int fd;
+	int n_wrote;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content)
+	/* if nothing to write, just return newly created file */
+	if (!text_content)
 	{
-		while (text_content[sl])
-			sl++;
-		t = write(fd, text_content, sl);
-		if (count != sl)
-			return (-10;
+		close(fd);
+		return (1);
+	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
-
 	return (1);
 }
