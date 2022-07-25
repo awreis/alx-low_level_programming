d0a7959e5df3bc0011da5e6a47c60231d2c847f5
Wareis <reiskofi@gmail.com>
2022-07-25 16:07:40 +0000

    create a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("filename", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 
@@ -42,5 +42,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	free(buf);
-	return(count);
+	return (count);
 }
