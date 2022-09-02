b60e4841f29ca0dea4bda3baa62be8ad0a7d8bba
Wareis <reiskofi@gmail.com>
2022-09-02 04:53:39 +0000

    Error fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ size_t dlistint_len(const dlistint_t *h)
 
 /**
  * delete_dnodeint_at_index - deltes a node in a doubly linked list
- * 				at a given index
+ * at a given index
  * @head: double pointer to the list
  * @index: index of the node to delete
  * Return: 1 on success & -1 on failure
