d60d1a23cb3af799ac795f5a912cc51f1ab02118
awreis <40414863+awreis@users.noreply.github.com>
2022-09-24 05:05:29 +0000

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: hash table to look into
- * @key: key of the value to retrieve
- * Return: the value associated with the element,
- * or NULL if key couldn’t be found
+ * hash_table_get -given key, get value
+ * @ht: hash table
+ * @key: key
+ * Return: value; or NULL if not found
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *node = NULL;
+	unsigned long int idx;
+	hash_node_t *tmp;
 
-	if (!ht || !key || !strcmp(key, ""))
+	/* find index in hash table where key is */
+	/* look through linked list to find matching key for value */
+
+	if (!ht || !key)
 		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = (ht->array)[index];
-
-	while (node)
-	{
-		if (!strcmp(node->key, (char *)key))
-			return (node->value);
-		node = node->next;
-	}
-	return (NULL);
+	tmp = (ht->array)[idx];
+	while (tmp != NULL && strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+	if (!tmp)
+		return (NULL);
+	else
+		return (tmp->value);
 }
